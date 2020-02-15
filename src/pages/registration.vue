6f8b27835f2cb9571ee7790e24dbb2f8819fc621
Marcel Hacker <china.pvpAT@gmx.at>
2020-02-15 12:41:11 +0100

    Add registration page, add route, add index.php

---
+++ src/pages/registration.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <h1>{{message}}</h1>
+    </div>
+</template>
+
+<script>
+export default {
+   // data: function(value, index) {return {message'hallo}}=> ({ message: 'Hallo' }),
+    data: () => ({ message: 'Hallo' }),
+    //data: {return {message: 'Hallo'}}
+};
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import Home from '@/pages/home.vue';
+import Registration from '@/pages/registration.vue';
 
 Vue.use(Router);
 
@@ -10,6 +11,11 @@ export default new Router({
             path: '/',
             name: 'Home',
             component: Home,
+        },
+        {
+            path: '/registration',
+            name: 'registration',
+            component: Registration,
         },
     ],
 });
