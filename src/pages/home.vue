328d827e154737a73866e5619575cddcd02e5d5c
Marcel Hacker <china.pvpAT@gmx.at>
2020-02-26 14:10:49 +0100

    Add photos for home page

--- src/pages/home.vue
+++ src/pages/home.vue
@@ -2,9 +2,9 @@
     <div>
         <navbar />
         <image-card
-            :title="'Frisch'"
-            :source="'https://picsum.photos/900/250/?image=3'"
-            :subtitle="'Hah'"
+            :title="'Ein erfolgreicher Tag, beginnt mit einem Frühstück'"
+            :source="'../assets/groceries/breakfast.jpg'"
+            :subtitle="'Gesund genießen'"
             :text="
                 'Voluptate magna ullamco irure exercitation duis est aute nisi ut. Esse incididunt tempor culpa et mollit elit sint labore pariatur dolore cillum irure ipsum culpa. Ex id eu mollit excepteur. Dolore aliqua veniam proident excepteur fugiat cillum non laboris aliqua ad sit aliquip.'
             "
